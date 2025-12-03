d8ef50d33fe24b0adb176bca361eab32d046016c
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-12-03 09:37:39 -0300

    Implementado butão flutuante que leva ao topo

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
-import { Footer } from "../footer/footer";
-import { Estoque } from "../estoque/estoque";
-import { ModalVender } from "../modal-vender/modal-vender";
-import { ModalInserir } from "../modal-inserir/modal-inserir";
-import { Vendas } from "../vendas/vendas";
-import { Location } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { Footer } from '../footer/footer';
+import { Estoque } from '../estoque/estoque';
+import { ModalVender } from '../modal-vender/modal-vender';
+import { ModalInserir } from '../modal-inserir/modal-inserir';
+import { Vendas } from '../vendas/vendas';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
-  imports: [Footer, Estoque, ModalVender, ModalInserir, Vendas],
+  imports: [Footer, Estoque, ModalVender, ModalInserir, Vendas, CommonModule],
 })
 export class Home {
+   mostrarBotaoTopo = false;
 
+  // Atualiza a variável quando o usuário rola a página
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY || window.pageYOffset;
+    this.mostrarBotaoTopo = scrollY > 200; // aparece após 200px de scroll
+  }
+
+  irParaTopo() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
